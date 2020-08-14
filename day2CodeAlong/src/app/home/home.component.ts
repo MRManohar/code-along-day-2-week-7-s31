4ce3f2f02d1a979dddf7578cecb650a64347c35b
MRManohar <lmmrider99@gmail.com>
2020-08-14 10:20:32 +0530

    FSJ0420A109

---
+++ day2CodeAlong/src/app/home/home.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  constructor() { }
+
+  clickCounter : number = 0;
+  ngOnInit(): void {
+  }
+
+  countClick(){
+    this.clickCounter += 1;
+  }
+}
